8f45c8bac671e7a40f0a02698a8c8db414e9e030
Ashley <anoelj86@gmail.com>
2023-07-16 11:49:32 -0500

    initial

--- App.js
+++ App.js
@@ -1,20 +1,180 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View,  Image, ScrollView,TextInput } from 'react-native';
 
-export default function App() {
+// const App = () => {
+//   return (
+//     <ScrollView>
+//       <Text>Some text</Text>
+//       <View>
+//         <Text>Some more text</Text>
+//         <Image
+//           source={{
+//             uri: 'https://www.sgsc.edu/content/userfiles/images/criminal%20justice.jpg',
+//           }}
+//           style={{width: 200, height: 200}}
+//         />
+//       </View>
+//       <TextInput
+//         style={{
+//           height: 40,
+//           borderColor: 'gray',
+//           borderWidth: 1,
+//         }}
+//         defaultValue="Type Here"
+//       />
+//     </ScrollView>
+//   );
+// };
+
+// export default App;
+
+// import React from 'react';
+// import {Image, ScrollView, Text} from 'react-native';
+
+// const logo = {
+//   uri: 'https://reactnative.dev/img/tiny_logo.png',
+//   width: 64,
+//   height: 64,
+// };
+
+// const App = () => (
+//   <ScrollView>
+//     <Text style={{fontSize: 96}}>Scroll me plz</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{fontSize: 96}}>If you like</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{fontSize: 96}}>Scrolling down</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{fontSize: 96}}>What's the best</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{fontSize: 96}}>Framework around?</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{fontSize: 80}}>React Native</Text>
+//   </ScrollView>
+// );
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+const FlexDirectionBasics = () => {
+  const [flexDirection, setflexDirection] = useState('column');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PreviewLayout
+      label="flexDirection"
+      values={['column', 'row', 'row-reverse', 'column-reverse']}
+      selectedValue={flexDirection}
+      setSelectedValue={setflexDirection}>
+      <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+    </PreviewLayout>
   );
-}
+};
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{padding: 10, flex: 1}}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map(value => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}>
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}>
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 8,
+    backgroundColor: 'aliceblue',
+  },
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: 'oldlace',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
+    marginBottom: 6,
+    minWidth: '48%',
+    textAlign: 'center',
+  },
+  selected: {
+    backgroundColor: 'coral',
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: 'coral',
+  },
+  selectedLabel: {
+    color: 'white',
+  },
+  label: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 24,
   },
 });
+
+export default FlexDirectionBasics;
